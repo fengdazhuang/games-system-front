a573abc3a51053da73daa4dedf5b77557d6fdef9
fengdazhuang <3172029811@qq.com>
2023-04-04 22:03:11 +0800

    second commit

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -12,7 +12,12 @@
                     <el-input type="password" :prefix-icon="Lock" v-model="user.password" />
                 </el-form-item>
               <el-form-item :style="{margin:'0 0 50px 0'}" prop="validateCode">
-                <el-input type="validateCode" :prefix-icon="Lock" v-model="user.validateCode" />
+                <el-input :style="{width:'60%'}" type="validateCode" :prefix-icon="Iphone" v-model="user.validateCode" />
+                <el-image
+                    :style="{width:'100px',height:'30px',margin:'0 0 0 20px',cursor:'pointer'}"
+                    :src="imgsrc"
+                    @click="handleChangeImage"
+                />
               </el-form-item>
                 <el-form-item>
                     <el-button type="primary" class="btn-login" @click="toLogin">登录</el-button>
@@ -24,7 +29,7 @@
 </template>
 
 <script>
-import { Avatar, Lock } from "@element-plus/icons-vue";
+import { Avatar, Lock,Iphone } from "@element-plus/icons-vue";
 import storage from '../utils/storage';
 export default {
     name: 'login',
@@ -36,11 +41,11 @@ export default {
               validateCode:'',
               key:''
             },
-
+            imgsrc:'',
             rules: {
                 username: [
                     {
-                        required: true, message: '请输入手机号', trigger: 'blur'
+                        required: true, message: '请输入用户名', trigger: 'blur'
                     }
                 ],
                 password: [
@@ -58,7 +63,8 @@ export default {
     },
   created(){
       this.$api.getValidateCode().then(res => {
-        console.log(res)
+        console.log('ress',res)
+        this.imgsrc= res.validateCode
         this.user.key = res.key
       })
   },
@@ -75,11 +81,16 @@ export default {
                     return false
                 }
             })
-        }
+        },
+      handleChangeImage(){
+        this.$api.getValidateCode().then(res => {
+          this.imgsrc= res.validateCode
+        })
+      }
     },
     setup () {
         return {
-            Avatar, Lock
+            Avatar, Lock,Iphone
         }
     }
 }
